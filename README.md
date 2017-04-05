e6bac74abf01ba906847f2995d1b1a6537abf6cf
Megan Warnock and Stormi Hoebelheinrich <megan@>
2017-04-05 09:12:56 -0700

    add initial project directories and files

---
+++ README.md
@@ -0,0 +1,29 @@
+# _Blog_
+
+#### _Blog, 04-05-2017_
+
+#### By _**Megan Warnock and Stormi Hoebelheinrich**_
+
+## Description
+_Example text for the description of the project_
+
+
+## Specifications
+
+| Behavior                   | Input Example     | Output Example    |
+| -------------------------- | -----------------:| -----------------:|
+
+
+
+## Setup/Installation Requirements
+
+* _Clone the repository_
+* _Run the command 'gradle run'_
+* _Open browser and go to localhost:4567_
+
+
+### License
+
+Copyright (c) 2017 **_Megan Warnock and Stormi Hoebelheinrich_**
+
+This software is licensed under the MIT license.
---
+++ src/test/java/DatabaseRule.java
@@ -0,0 +1,24 @@
+import org.junit.rules.ExternalResource;
+import org.sql2o.*;
+
+public class DatabaseRule extends ExternalResource {
+
+  @Override
+  protected void before() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/blog_test", null, null);
+  }
+
+  @Override
+  protected void after() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteUserQuery = "DELETE FROM users *;";
+      con.createQuery(deleteUserQuery).executeUpdate();
+      String deleteCommentQuery = "DELETE FROM comments *;";
+      con.createQuery(deleteCommentQuery).executeUpdate();
+      String deleteTagQuery = "DELETE FROM tags *;";
+      con.createQuery(deleteTagQuery).executeUpdate();
+      String deletePostQuery = "DELETE FROM posts *;";
+      con.createQuery(deletePostQuery).executeUpdate();
+    }
+  }
+}
